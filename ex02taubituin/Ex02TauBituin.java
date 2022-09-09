68d81b228243168eff20eaa18e2a58566ca21a55
ajbtwn <93103779+ajbtwn@users.noreply.github.com>
2022-09-09 12:16:21 +0800

    Add files via upload

---
+++ ex02taubituin/Ex02TauBituin.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ex02taubituin;
+
+/**
+ *
+ * @author TAU
+ */
+public class Ex02TauBituin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Welcome to Higher or Lower! What will you do?\n-Start game\n-Change settings\n-End application");
+        String choice = sc.nextLine();
+    }
+    
+}
